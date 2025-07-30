5f66daa9b78b6a704ebf5f05102b1bc2bba85f28
Dan Camina <d.camina91@gmail.com>
2025-07-30 16:59:46 +0100

    feat: project init

---
+++ setupTests.js
@@ -0,0 +1,17 @@
+import { beforeAll, afterEach, afterAll, vi } from "vitest";
+import { server } from "./src/mocks/server";
+
+window.alert = vi.fn(); // Mock alert function
+
+// Start the server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+// Reset any request handlers that are declared as a part of our tests
+// (i.e. for testing one-time error scenarios)
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers());
